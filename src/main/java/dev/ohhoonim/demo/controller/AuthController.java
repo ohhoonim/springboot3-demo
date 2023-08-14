670057df3a1167803b13598e98e4a329a1495715
주형돈 <hdju@rntime.com>
2023-08-14 17:50:15 +0900

    refresh token

--- src/main/java/dev/ohhoonim/demo/controller/AuthController.java
+++ src/main/java/dev/ohhoonim/demo/controller/AuthController.java
@@ -1,5 +1,6 @@
 package dev.ohhoonim.demo.controller;
 
+import java.io.IOException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,8 @@ import dev.ohhoonim.demo.controller.dto.AuthRequest;
 import dev.ohhoonim.demo.controller.dto.AuthResponse;
 import dev.ohhoonim.demo.model.secondary.User;
 import dev.ohhoonim.demo.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,18 +21,18 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    // public ResponseEntity<User> authenticate(@RequestBody User user) {
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest login) {
         User user = User.builder()
             .email(login.email()) 
             .password(login.password())
             .build(); 
-        
         return ResponseEntity.ok(authService.authenticate(user));
-
         // authority : 회원가입 , 인가 
         // authentication : 로그인 , 인증 
-        // auth 
+    }
 
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authService.refreshToken(request, response);
     }
 }
--- src/main/java/dev/ohhoonim/demo/service/AuthService.java
+++ src/main/java/dev/ohhoonim/demo/service/AuthService.java
@@ -1,11 +1,21 @@
 package dev.ohhoonim.demo.service;
 
+import java.io.IOException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.net.HttpHeaders;
 import dev.ohhoonim.demo.config.service.JwtService;
+import dev.ohhoonim.demo.controller.dto.AuthRequest;
 import dev.ohhoonim.demo.controller.dto.AuthResponse;
 import dev.ohhoonim.demo.model.secondary.User;
+import dev.ohhoonim.demo.repository.secondary.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,13 +23,33 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
-    
+    private final UserRepository userRepository;
+
     // public User authenticate(User user) {
     public AuthResponse authenticate(User user) {
-       authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword())
-       ) ;
-       return new AuthResponse(jwtService.generateToken(user));
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+        return new AuthResponse(jwtService.generateToken(user),
+                jwtService.generateRefreshToken(user));
     }
-    
+
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String userEmail; // username
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        refreshToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(refreshToken);
+        if (userEmail != null) {
+            User user= this.userRepository.findByEmail(userEmail).get();
+            if (jwtService.isTokenValid(refreshToken, user)) {
+               String accessToken = jwtService.generateToken(user);
+               AuthResponse authResponse = new AuthResponse(accessToken, refreshToken);
+                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+            }
+        }
+    }
+
 }
